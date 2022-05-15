853c9fab3c6784438e29a28ef7402489f8dcfa76
Christina R <xsnachannell@gmail.com>
2022-05-14 18:00:21 -0700

    updates index.js name in package.json, updates readme with additional resource, updates index.js so that menu works to print out departments, roles and employees. Still need to delete uneccessary code and fix so that application adds new employee, adds new role and updates an existing employee's role

--- README.md
+++ README.md
@@ -19,3 +19,5 @@ MIT
 - [Foreign Keys Referencing Same Table](https://stackoverflow.com/questions/18680680/can-a-foreign-key-refer-to-a-primary-key-in-the-same-table)
 
 - [console.table npm package](https://www.npmjs.com/package/console.table)
+
+- [Sequelize Queries](https://sequelize.org/docs/v6/core-concepts/raw-queries/)
--- index.js
+++ index.js
@@ -1,187 +1,198 @@
 
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
+// const mysql = require('mysql2'); dont think I need this if I'm using sequelize...
 const cTable = require('console.table')
+const sequelize = require("./config/connection");
 
 //copies, updates, and refactors code from my teamProfile Generator https://github.com/ChannellNumber5/CR-TeamProfileGenerator
-function init(){
+    
+function runMenu() {
+    inquirer
+    .prompt([
+        {type:"list",
+        name: "toDoNext",
+        message: "Please select from the following options:",
+        choices:["View all Departments", "View all Roles", "View all Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role", "Exit Application"]
+        }
+    ])
+    .then(async (data) => {
+        if(data.toDoNext === "View all Departments") {
+           console.table(await viewAllDepartments());
+            runMenu();
+        } else if (data.toDoNext === "View all Roles") {
+            console.table(await viewAllRoles());
+            runMenu();
+        } else if (data.toDoNext === "View all Employees") {
+            console.table(await viewAllEmployees());
+            runMenu();
+        } else if (data.toDoNext === "Add a Department") {
+            addDepartment();
+            runMenu();
+        } else if (data.toDoNext === "Add a Role") {
+            addRole();
+            runMenu();
+        } else if (data.toDoNext === "Add an Employee") {
+            addEmployee();
+            runMenu();
+        } else if (data.toDoNext === "Update an Employee Role") {
+            updateEmployee();
+            runMenu();
+        } else {
+            console.log("Thank you for using the Employee Tracker Application!")
+        }
+    });
+
+}
+
+async function viewAllDepartments() {
+    const deptData = await sequelize.query("SELECT * FROM departments");
+        const tableData = cTable.getTable(deptData[1]);
+        return tableData;
+}
+
+async function viewAllRoles() {
+    const rolesData = await sequelize.query("SELECT * FROM roles");
+    const tableData = cTable.getTable(rolesData[1]);
+    return tableData;
+}
+
+async function viewAllEmployees() {
+    const empData = await sequelize.query("SELECT * FROM employees");
+    const tableData = cTable.getTable(empData[1]);
+    return tableData;
+}
+
+
+function addDepartment() {
     inquirer
         .prompt([
             {type:"input",
-            name: "managerName",
-            message: "Hello Team Manager! Please Enter your Name:"
-            },
-            {type:"number",
-            name: "managerId",
-            message: "Please Enter your Employee ID Number:"
-            },
-            {type:"input",
-            name: "managerEmail",
-            message: "Please Enter your Email Address:"
-            },
-            {type:"number",
-            name: "managerOfficeNumber",
-            message: "Please Enter your Office Number:"
+            name: "newDept",
+            message: "Creating New Department \n Please Enter New Department Name:"
             },
         ])
         .then((data) => {
-            if (data.managerName === undefined || data.managerId === undefined || data.managerEmail === undefined|| data.managerOfficeNumber === undefined){
-                console.log(data)
-                console.log("Please rerun script and input correct data for name, ID, email and Office Number");
-                init();
-            } else {
-                const manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.managerOfficeNumber)
-                teamList.push(manager);
-                teamMenu();
-            }
+            // const newDept = new Intern(data.internName, data.internId, data.internEmail, data.school)
+            // teamList.push(intern);
+            // teamMenu();
         })
-    }
-    
-    function runMenu() {
-        inquirer
+}
+
+function addRole() {
+    inquirer
         .prompt([
+            {type:"input",
+            name: "title",
+            message: "Creating Role \n Please Enter Role Title:"
+            },
+            {type:"number",
+            name: "salary",
+            message: "What is the salary for this role?"
+            },
             {type:"list",
-            name: "toDoNext",
-            message: "Please select from the following options:",
-            choices:["View all Departments", "View all Roles", "View all Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"]
-            }
-        ])
-        .then((data) => {
-            if(data.toDoNext === "View all Departments") {
-                viewAllDepartments();
-            } else if (data.toDoNext === "View all Roles") {
-                viewAllRoles();
-            } else if (data.toDoNext === "View all Employees") {
-                viewAllEmployees();
-            } else if (data.toDoNext === "Add a Department") {
-                addDepartment();
-            } else if (data.toDoNext === "Add a Role") {
-                addRole();
-            } else if (data.toDoNext === "Add an Employee") {
-                addEmployee();
-            } else if (data.toDoNext === "Update an Employee Role") {
-                updateEmployee();
-            } else {
-                console.log("Thank you for using the Employee Tracker Application!")
-            }
-        });
-    
-    }
-
-    function viewAllDepartments(){
-
-    }
-    
-    function createIntern() {
-        inquirer
-            .prompt([
-                {type:"input",
-                name: "internName",
-                message: "New Intern Employee Card Creation Mode: \n Please Enter the Intern's Name:"
-                },
-                {type:"number",
-                name: "internId",
-                message: "Please Enter the Intern's Employee ID Number:"
-                },
-                {type:"input",
-                name: "internEmail",
-                message: "Please Enter the Intern's Email Address:"
-                },
-                {type:"input",
-                name: "school",
-                message: "Please Enter the Intern's Current School/University:"
-                },
-            ])
-            .then((data) => {
-                const intern = new Intern(data.internName, data.internId, data.internEmail, data.school)
-                teamList.push(intern);
-                teamMenu();
-            })
-    }
-    
-    function createEngineer() {
-        inquirer
-            .prompt([
-                {type:"input",
-                name: "engName",
-                message: "New Engineer Employee Card Creation Mode: \n Please Enter the Engineer's Name:"
-                },
-                {type:"number",
-                name: "engId",
-                message: "Please Enter the Engineer's Employee ID Number:"
-                },
-                {type:"input",
-                name: "engEmail",
-                message: "Please Enter the Engineer's Email Address:"
-                },
-                {type:"input",
-                name: "github",
-                message: "Please Enter the Engineer's Github Username:"
-                },
-            ])
-            .then((data) => {
-                const engineer = new Engineer(data.engName, data.engId, data.engEmail, data.github)
-                teamList.push(engineer);
-                teamMenu();
-            });
-    }
-    
-    function deleteEmployee() {
-        inquirer
-        .prompt([
-            {type: "list",
-            name: "toDelete",
-            message: "Which Team Member would you like to Delete?",
-            choices: teamList}
-        ])
-        .then(async (data) => {
-            for(let i = 0; i < teamList.length; i++) {
-                if (i !== 0 && data[i] === data.toDelete) {
-                    teamList.splice(i, 1);
-                    teamMenu();
-                    return;
-                } else if (i === 0 && data[i] === data.toDelete) {
-                   await changeManager();
-                }
-            }
-        })
-    }
-    
-    function changeManager() {
-        inquirer
-        .prompt([
-            {type:"input",
-            name: "managerName",
-            message: "Reacreating Team Manager Profile! \n Please Enter your Name:"
-            },
-            {type:"number",
-            name: "managerId",
-            message: "Please Enter your Employee ID Number:"
-            },
-            {type:"input",
-            name: "managerEmail",
-            message: "Please Enter your Email Address:"
-            },
-            {type:"number",
-            name: "managerOfficeNumber",
-            message: "Please Enter your Office Number:"
+            name: "roleDept",
+            message: "What department does this role belong to?",
+            choices:[]
             },
         ])
         .then((data) => {
-            if (data.managerName === undefined || data.managerId === undefined || data.managerEmail === undefined|| data.managerOfficeNumber === undefined){
-                console.log(data)
-                console.log("Please input correct data for name, ID, email and Office Number");
-                changeManager();
-            } else {
-                teamList[1].name = data.managerName;
-                teamList[1].id = data.managerId;
-                teamList[1].email = data.managerEmail;
-                teamList[1].officeNumber = data.managerOfficeNumber;
-    
-                teamMenu();
-            }
+            // const newDept = new Intern(data.internName, data.internId, data.internEmail, data.school)
+            // teamList.push(intern);
+            // teamMenu();
         })
-    }
+}
+function addEmployee() {
+    inquirer
+        .prompt([
+            {type:"input",
+            name: "first_name",
+            message: "Adding New Employee \n Please Enter Employee's First Name:"
+            },
+            {type:"input",
+            name: "last_name",
+            message: "Adding New Employee \n Please Enter Employee's Last Name:"
+            },
+            {type:"list",
+            name: "role",
+            message: "What is this employees role?",
+            choices:[]
+            },
+            {type:"list",
+            name: "manager",
+            message: "Who is this employee's manager?",
+            choices:[]
+            },
+            {type:"input",
+            name: "school",
+            message: "Please Enter the Intern's Current School/University:"
+            },
+        ])
+        .then((data) => {
+            const newDept = new Intern(data.internName, data.internId, data.internEmail, data.school)
+            teamList.push(intern);
+            teamMenu();
+        })
+}
 
-    
-    init();
+async function currentRoles () {
+    const roles = await sequelize.query('SELECT title FROM roles')
+}
+
+function deleteEmployee() {
+    inquirer
+    .prompt([
+        {type: "list",
+        name: "toDelete",
+        message: "Which Team Member would you like to Delete?",
+        choices: teamList}
+    ])
+    .then(async (data) => {
+        for(let i = 0; i < teamList.length; i++) {
+            if (i !== 0 && data[i] === data.toDelete) {
+                teamList.splice(i, 1);
+                teamMenu();
+                return;
+            } else if (i === 0 && data[i] === data.toDelete) {
+                await changeManager();
+            }
+        }
+    })
+}
+
+function changeManager() {
+    inquirer
+    .prompt([
+        {type:"input",
+        name: "managerName",
+        message: "Reacreating Team Manager Profile! \n Please Enter your Name:"
+        },
+        {type:"number",
+        name: "managerId",
+        message: "Please Enter your Employee ID Number:"
+        },
+        {type:"input",
+        name: "managerEmail",
+        message: "Please Enter your Email Address:"
+        },
+        {type:"number",
+        name: "managerOfficeNumber",
+        message: "Please Enter your Office Number:"
+        },
+    ])
+    .then((data) => {
+        if (data.managerName === undefined || data.managerId === undefined || data.managerEmail === undefined|| data.managerOfficeNumber === undefined){
+            console.log(data)
+            console.log("Please input correct data for name, ID, email and Office Number");
+            changeManager();
+        } else {
+            teamList[1].name = data.managerName;
+            teamList[1].id = data.managerId;
+            teamList[1].email = data.managerEmail;
+            teamList[1].officeNumber = data.managerOfficeNumber;
+
+            teamMenu();
+        }
+    })
+}
+
+runMenu();
